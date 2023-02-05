b9de5490cd517c67cd128f932cf97ee252372d1d
Gregory Kurkin <monstercondo@gmail.com>
2023-02-05 18:23:56 +0400

    update structure of Brain-calc

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,14 +1,24 @@
-import prompt
 from random import randint, choice
-from brain_games.functions import calc
+import operator
 
 
-def game_calc():
+def calc(a, used_operator, b):
+    operators_dict = {'+': operator.add(a, b),
+                      '-': operator.sub(a, b),
+                      '*': operator.mul(a, b)}
+    return operators_dict[used_operator]
+
+
+def game_task():
+    task = 'What is the result of the expression?'
+    return task
+
+
+def game_logik():
     a = randint(0, 10)
     b = randint(0, 10)
     operators = '+-*'
     used_operator = choice(operators)
-    print(f'Question: {a} {used_operator} {b}')
-    func_result = calc(a, used_operator, b)
-    answer = prompt.string('Your answer: ')
-    return (answer, func_result)
+    question = f'Question: {a} {used_operator} {b}'
+    func_result = str(calc(a, used_operator, b))
+    return (question, func_result)
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
+from brain_games.games import game_calc
+from brain_games.game_engine import game_engine
 
 
-import prompt
-from brain_games.games.game_calc import game_calc
-
-
-def main():  # brain_calc.py
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    # start game
-    # print('Answer "yes" if the number is even, otherwise answer "no".')
-    print('What is the result of the expression?')
-    counter = 0
-    while counter != 3:
-        answer, func_result = game_calc()
-        if answer != str(func_result):
-            print(f"'{answer}' is wrong answer ;(. Correct answer was "
-                  f"'{func_result}'. \nLet's try again, {name}!")
-            break
-        else:
-            print('Correct!')
-            counter += 1
-    if counter == 3:
-        print(f'Congratulations, {name}!')
+def main():
+    game_engine(game_calc)
 
 
 if __name__ == '__main__':
