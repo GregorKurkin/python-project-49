c47d7141fb90e9c374892b7890393da3006f309c
Gregory Kurkin <monstercondo@gmail.com>
2023-02-11 11:17:35 +0300

    update function in game-even

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,11 +1,8 @@
 from random import randint
 
 
-def if_even(question_number):
-    if question_number % 2 == 0:
-        return 'yes'
-    else:
-        return 'no'
+def is_even(question_number):
+    return question_number % 2 == 0
 
 
 def game_task():
@@ -15,6 +12,5 @@ def game_task():
 
 def game_logik():
     question = randint(-1000, 1000)
-    # question = f'Question: {question_number}'
-    func_result = if_even(question)
+    func_result = (is_even(question) and 'yes' or 'no')
     return (question, func_result)
